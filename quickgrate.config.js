c25d32b86b18ce5be20dfc1fa5b7e606dde595cb
Máriás Tibor <tibormarias@gmail.com>
2022-12-17 13:42:39 +0100

    config file example

---
+++ quickgrate.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    master: {
+        host: "127.0.0.1",
+        port: "3306",
+        user: "user",
+        password: "pass",
+        database: "production",
+    },
+    slave: {
+        host: "127.0.0.1",
+        port: "3306",
+        user: "user",
+        password: "pass",
+        database: "development"
+    },
+    rows_limit: 25000,
+    tables: {
+        do_not_create: [],
+        do_not_seed: []
+    }
+}
